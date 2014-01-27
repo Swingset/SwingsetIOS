332bb1464062ae8fc5832ba24c83289fb21b30b7
swingset <swingsetapp@gmail.com>
2014-01-26 23:49:09 -0500

    set up basic view hierarchy for QuestionsVC

---
+++ SwingSet/Questions/SSQuestionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSQuestionsViewController.h
+//  SwingSet
+//
+//  Created by Denny Kwon on 1/26/14.
+//  Copyright (c) 2014 SwingSet Labs. All rights reserved.
+//
+
+#import "SSViewController.h"
+
+@interface SSQuestionsViewController : SSViewController
+
+@end
