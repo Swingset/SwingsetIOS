e9bb62141e8ac8901b6899bf2d6ca8eff9422224
swingset <swingsetapp@gmail.com>
2014-01-26 23:49:34 -0500

    created uiview subclass for question preview

---
+++ SwingSet/Questions/SSQuestionPreview.h
@@ -0,0 +1,22 @@
+//
+//  SSQuestionPreview.h
+//  SwingSet
+//
+//  Created by Denny Kwon on 1/26/14.
+//  Copyright (c) 2014 SwingSet Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol SSQuestionPreviewDelegate <NSObject>
+- (void)checkPostion;
+@end
+
+@interface SSQuestionPreview : UIView
+
+@property (assign) id delegate;
+@property (strong, nonatomic) UILabel *lblText;
+@property (strong, nonatomic) UILabel *lblDate;
+@property (strong, nonatomic) UILabel *lblVotes;
+@property (strong, nonatomic) UIImageView *imageView;
+@end
---
+++ SwingSet/Questions/SSQuestionPreview.m
@@ -0,0 +1,160 @@
+//
+//  SSQuestionPreview.m
+//  SwingSet
+//
+//  Created by Denny Kwon on 1/26/14.
+//  Copyright (c) 2014 SwingSet Labs. All rights reserved.
+//
+
+#import "SSQuestionPreview.h"
+#import "Config.h"
+#import <QuartzCore/QuartzCore.h>
+
+
+CGFloat randomRGB(){
+    UInt32 i = arc4random() % 256;
+    i = abs(i);
+    CGFloat rgb = i/255.0f;
+    return rgb;
+}
+
+@interface SSQuestionPreview ()
+@property (nonatomic) CGPoint startPoint;
+@end
+
+@implementation SSQuestionPreview
+@synthesize delegate;
+@synthesize lblText;
+@synthesize imageView;
+
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [self getUIColorObjectFromHexString:@"#f9f9f9" alpha:1];
+        self.layer.masksToBounds = YES;
+        self.layer.cornerRadius = 4.0f;
+        self.layer.borderWidth = 1.0f;
+        self.layer.borderColor = [[UIColor colorWithRed:0.44f green:0.44f blue:0.44f alpha:1] CGColor];
+        
+        CGFloat iconDimen = 100.0f;
+        CGFloat y = 0.0f;
+        self.lblText = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, y, frame.size.width-iconDimen, iconDimen)];
+        self.lblText.backgroundColor = [UIColor colorWithRed:randomRGB() green:randomRGB() blue:randomRGB() alpha:1.0f];;
+        self.lblText.numberOfLines = 0;
+        self.lblText.lineBreakMode = NSLineBreakByWordWrapping;
+        self.lblText.font = [UIFont fontWithName:@"ProximaNova-Black" size:16.0f];
+        self.lblText.textAlignment = NSTextAlignmentCenter;
+        self.lblText.text = @"This is the question";
+        self.lblText.textColor = kGreenNext;
+        [self addSubview:self.lblText];
+        y += iconDimen;
+        
+        CGFloat w = 0.5*frame.size.width;
+        self.lblDate = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, y, w, 20.0f)];
+        self.lblDate.backgroundColor = [UIColor colorWithRed:0.8f green:0.8f blue:0.8f alpha:1.0f];
+        self.lblDate.text = @" Jan 26";
+        self.lblDate.font = [UIFont fontWithName:@"ProximaNova-RegularIt" size:12.0f];
+        [self addSubview:self.lblDate];
+
+        self.lblVotes = [[UILabel alloc] initWithFrame:CGRectMake(w, y, w, 20.0f)];
+        self.lblVotes.font = self.lblDate.font;
+        self.lblVotes.textAlignment = NSTextAlignmentRight;
+        self.lblVotes.text = @"15 votes ";
+        self.lblVotes.backgroundColor = self.lblDate.backgroundColor;
+        [self addSubview:self.lblVotes];
+
+        self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(frame.size.width-iconDimen, 0.0f, iconDimen, iconDimen)];
+        self.imageView.backgroundColor = [UIColor blackColor];
+        self.imageView.image = [UIImage imageNamed:@"placeholder.png"];
+        [self addSubview:self.imageView];
+        
+        
+        
+    }
+    return self;
+}
+
+
+- (UIColor *)getUIColorObjectFromHexString:(NSString *)hexStr alpha:(CGFloat)alpha
+{
+    // Convert hex string to an integer
+    unsigned int hexint = [self intFromHexString:hexStr];
+    
+    // Create color object, specifying alpha as well
+    UIColor *color =
+    [UIColor colorWithRed:((CGFloat) ((hexint & 0xFF0000) >> 16))/255
+                    green:((CGFloat) ((hexint & 0xFF00) >> 8))/255
+                     blue:((CGFloat) (hexint & 0xFF))/255
+                    alpha:alpha];
+    
+    return color;
+}
+
+
+
+- (unsigned int)intFromHexString:(NSString *)hexStr
+{
+    unsigned int hexInt = 0;
+    
+    // Create scanner
+    NSScanner *scanner = [NSScanner scannerWithString:hexStr];
+    
+    // Tell scanner to skip the # character
+    [scanner setCharactersToBeSkipped:[NSCharacterSet characterSetWithCharactersInString:@"#"]];
+    
+    // Scan hex value
+    [scanner scanHexInt:&hexInt];
+    
+    return hexInt;
+}
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+#pragma mark - UIResponder
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+//    NSLog(@"touchesBegan:");
+    UITouch *touch = [touches anyObject];
+    self.startPoint = [touch locationInView:self.superview];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint p = [touch locationInView:self.superview];
+    CGFloat delta = p.x-self.startPoint.x;
+//    NSLog(@"touchesMoved: %.2f", delta);
+
+    self.center = CGPointMake(self.center.x+delta, self.center.y);
+
+    self.startPoint = p;
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+//    NSLog(@"touchesEnded:");
+    
+    if ([self.delegate respondsToSelector:@selector(checkPostion)])
+        [self.delegate checkPostion];
+    
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+//    NSLog(@"touchesCancelled:");
+    
+}
+
+
+
+@end
