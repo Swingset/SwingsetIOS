2054c287c5b5f23316f910056a537f8c0a573021
Denny Kwon <info@tsunami234.com>
2014-02-01 04:18:55 -0500

    new view controller for creating or joining a group.

---
+++ SwingSet/Groups/SSCreateGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSCreateGroupViewController.h
+//  SwingSet
+//
+//  Created by Denny Kwon on 2/1/14.
+//  Copyright (c) 2014 SwingSet Labs. All rights reserved.
+//
+
+#import "SSViewController.h"
+
+@interface SSCreateGroupViewController : SSViewController <UITextFieldDelegate>
+
+@end
---
+++ SwingSet/Groups/SSCreateGroupViewController.m
@@ -0,0 +1,229 @@
+//
+//  SSCreateGroupViewController.m
+//  SwingSet
+//
+//  Created by Denny Kwon on 2/1/14.
+//  Copyright (c) 2014 SwingSet Labs. All rights reserved.
+//
+
+#import "SSCreateGroupViewController.h"
+
+@interface SSCreateGroupViewController ()
+@property (strong, nonatomic) SSTextField *groupPWField;
+@property (strong, nonatomic) SSTextField *groupNameField;
+@property (strong, nonatomic) SSTextField *joinGroupNameField;
+@end
+
+@implementation SSCreateGroupViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.edgesForExtendedLayout = UIRectEdgeAll;
+        
+    }
+    return self;
+}
+
+- (void)loadView
+{
+    UIView *view = [self baseView:YES];
+    CGRect frame = view.frame;
+    
+    
+    CGFloat h = 0.40f*frame.size.height;
+    UIView *bgTop = [SSContentBackground backgroundWithFrame:CGRectMake(-5.0f, 16.0f, frame.size.width+10.0f, h)];
+    bgTop.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    [view addSubview:bgTop];
+    
+    CGFloat padding = 0.075f*h;
+    CGFloat y = padding;
+    UILabel *lblJoin = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, y, frame.size.width, 40.0f)];
+    lblJoin.font = [UIFont fontWithName:@"ProximaNova-Bold" size:24.0f];
+    lblJoin.text = @"Join An Existing Group";
+    lblJoin.textAlignment = NSTextAlignmentCenter;
+    lblJoin.textColor = [UIColor blackColor];
+    lblJoin.backgroundColor = [UIColor clearColor];
+    [bgTop addSubview:lblJoin];
+    y += lblJoin.frame.size.height+padding;
+    
+    CGFloat w = 0.7f*frame.size.width;
+    h = 36.0f;
+    self.joinGroupNameField = [SSTextField textFieldWithFrame:CGRectMake(0.5f*(frame.size.width-w), y, w, h)
+                                       placeholder:@"Enter Group Username"
+                                          keyboard:UIKeyboardTypeAlphabet];
+    
+    self.joinGroupNameField.delegate = self;
+    self.joinGroupNameField.text = self.profile.phone;
+    [bgTop addSubview:self.joinGroupNameField];
+    
+    UILabel *atLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.joinGroupNameField.frame.origin.x-h-2.0f, y, h, h)];
+    atLabel.text = @"@";
+    atLabel.textAlignment = NSTextAlignmentRight;
+    [bgTop addSubview:atLabel];
+    y += h+padding;
+    
+    w = 0.5f*frame.size.width;
+    SSButton *btnJoin = [SSButton buttonWithFrame:CGRectMake(0.5f*(frame.size.width-w), y, w, h) title:@"Join Group" textMode:TextModeUpperCase];
+    btnJoin.backgroundColor = kGreenNext;
+    [bgTop addSubview:btnJoin];
+    
+    
+    
+    // - - - - - - - - - - - - - - - - - - - - - - - BOTTOM - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+    
+    
+    
+    y = bgTop.frame.origin.y+bgTop.frame.size.height+25.0f;
+    h = 0.55f*frame.size.height;
+    UIView *bgBottom = [SSContentBackground backgroundWithFrame:CGRectMake(-5.0f, y, frame.size.width+10.0f, h)];
+    bgBottom.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    [view addSubview:bgBottom];
+    
+    y = padding;
+    UILabel *lblCreate = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, y, frame.size.width, 40.0f)];
+    lblCreate.font = [UIFont fontWithName:@"ProximaNova-Bold" size:24.0f];
+    lblCreate.text = @"Create A New Group";
+    lblCreate.textAlignment = NSTextAlignmentCenter;
+    lblCreate.textColor = [UIColor blackColor];
+    lblCreate.backgroundColor = [UIColor clearColor];
+    [bgBottom addSubview:lblCreate];
+    y += lblCreate.frame.size.height+padding;
+    
+
+    
+    w = 0.7f*frame.size.width;
+    h = 36.0f;
+    self.groupNameField = [SSTextField textFieldWithFrame:CGRectMake(0.5f*(frame.size.width-w), y, w, h)
+                                                    placeholder:@"Enter New Group Username"
+                                                       keyboard:UIKeyboardTypeAlphabet];
+    self.groupNameField.delegate = self;
+    [bgBottom addSubview:self.groupNameField];
+
+    atLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.groupNameField.frame.origin.x-h-2.0f, y, h, h)];
+    atLabel.text = @"@";
+    atLabel.textAlignment = NSTextAlignmentRight;
+    [bgBottom addSubview:atLabel];
+    y += h+padding;
+
+    self.groupPWField = [SSTextField textFieldWithFrame:CGRectMake(0.5f*(frame.size.width-w), y, w, h)
+                                                         placeholder:@"New Group Password"
+                                                            keyboard:UIKeyboardTypeAlphabet];
+    self.groupPWField.delegate = self;
+    [bgBottom addSubview:self.groupPWField];
+    y += h+padding;
+    
+    w = 0.5f*frame.size.width;
+    SSButton *btnInvite = [SSButton buttonWithFrame:CGRectMake(0.5f*(frame.size.width-w), y, w, h) title:@"Invite" textMode:TextModeUpperCase];
+    btnInvite.backgroundColor = kGreenNext;
+    [btnInvite addTarget:self action:@selector(btnInviteAction:) forControlEvents:UIControlEventTouchUpInside];
+    [bgBottom addSubview:btnInvite];
+
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(shiftBack)];
+    [view addGestureRecognizer:tap];
+
+    
+    self.view = view;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    SSNavigationController *navController = (SSNavigationController *)self.navigationController;
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"menu"
+                                                                             style:UIBarButtonItemStylePlain
+                                                                            target:navController
+                                                                            action:@selector(toggle)];
+    
+}
+
+
+
+- (void)shiftUp
+{
+    NSLog(@"SHIFT UP! !");
+    if (self.view.frame.origin.y < 0)
+        return;
+    
+    [UIView animateWithDuration:0.25f
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         CGRect frame = self.view.frame;
+                         frame.origin.y = -160.0f;
+                         self.view.frame = frame;
+                     }
+                     completion:^(BOOL finished){
+                         
+                     }];
+
+}
+
+- (void)shiftBack
+{
+    [self.groupNameField resignFirstResponder];
+    [self.groupPWField resignFirstResponder];
+    [self.joinGroupNameField resignFirstResponder];
+    
+    if (self.view.frame.origin.y == 0)
+        return;
+
+
+    [UIView animateWithDuration:0.25f
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         CGRect frame = self.view.frame;
+                         frame.origin.y = 0.0f;
+                         self.view.frame = frame;
+                     }
+                     completion:^(BOOL finished){
+                         
+                     }];
+
+}
+
+- (void)btnInviteAction:(SSButton *)btn
+{
+    NSLog(@"btnInviteAction:");
+    [self shiftBack];
+    
+    if (self.groupNameField.text.length < 5 || self.groupNameField.text.length > 15){
+        [self showAlert:@"Invalid Group Name" withMessage:@"Please enter a valid group name (5 to 15 characters)."];
+        return;
+    }
+    
+    if(self.groupPWField.text.length!=4){
+        [self showAlert:@"Invalid PIN Number" withMessage:@"Please enter a valid pin number (4 characters)."];
+        return;
+    }
+    
+}
+
+#pragma mark - UITextFieldDelegate
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    NSLog(@"textFieldShouldBeginEditing: %.2f", textField.frame.origin.y );
+    if ([textField isEqual:self.groupPWField]==YES || [textField isEqual:self.groupNameField]==YES){
+        [self shiftUp];
+        
+//        NSLog(@"SHIFT UP! !");
+    }
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self shiftBack];
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
