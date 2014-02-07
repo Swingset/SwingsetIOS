408c7dc81c227d6ea00f599c6b16867de145fc0c
Denny Kwon <info@tsunami234.com>
2014-02-07 02:28:09 -0500

    UIView category for converting html hex codes to uicolors

---
+++ SwingSet/BaseClasses/UIView+SSView.h
@@ -0,0 +1,15 @@
+//
+//  UIView+SSView.h
+//  SwingSet
+//
+//  Created by Denny Kwon on 2/7/14.
+//  Copyright (c) 2014 SwingSet Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (SSView)
+
+
+- (UIColor *)getUIColorObjectFromHexString:(NSString *)hexStr alpha:(CGFloat)alpha;
+@end
--- SwingSet/Questions/SSQuestionPreview.m
+++ SwingSet/Questions/SSQuestionPreview.m
@@ -9,6 +9,7 @@
 #import "SSQuestionPreview.h"
 #import "Config.h"
 #import "SSOptionView.h"
+#import "UIView+SSView.h"
 #import <QuartzCore/QuartzCore.h>
 
 #define kPadding 10.0f
@@ -218,38 +219,6 @@ CGFloat randomRGB(){
     [self.delegate viewComments];
 }
 
-- (UIColor *)getUIColorObjectFromHexString:(NSString *)hexStr alpha:(CGFloat)alpha
-{
-    // Convert hex string to an integer
-    unsigned int hexint = [self intFromHexString:hexStr];
-    
-    // Create color object, specifying alpha as well
-    UIColor *color =
-    [UIColor colorWithRed:((CGFloat) ((hexint & 0xFF0000) >> 16))/255
-                    green:((CGFloat) ((hexint & 0xFF00) >> 8))/255
-                     blue:((CGFloat) (hexint & 0xFF))/255
-                    alpha:alpha];
-    
-    return color;
-}
-
-
-
-- (unsigned int)intFromHexString:(NSString *)hexStr
-{
-    unsigned int hexInt = 0;
-    
-    // Create scanner
-    NSScanner *scanner = [NSScanner scannerWithString:hexStr];
-    
-    // Tell scanner to skip the # character
-    [scanner setCharactersToBeSkipped:[NSCharacterSet characterSetWithCharactersInString:@"#"]];
-    
-    // Scan hex value
-    [scanner scanHexInt:&hexInt];
-    
-    return hexInt;
-}
 
 - (void)reset
 {
