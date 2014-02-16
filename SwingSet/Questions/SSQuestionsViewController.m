4ff8e7932eaf4d57e5f166a423e0d14643be8c02
Denny Kwon <info@tsunami234.com>
2014-02-15 19:38:45 -0500

    show correct number of votes.

--- SwingSet/Questions/SSQuestionsViewController.m
+++ SwingSet/Questions/SSQuestionsViewController.m
@@ -305,6 +305,7 @@
 {
     preview.lblText.text = question.text;
     preview.imageView.image = (question.image) ? question.image : nil;
+    preview.lblVotes.text = [NSString stringWithFormat:@"%d votes", (int)question.votes.count];
 
     if ([question.answerType isEqualToString:@"text"]) {
         for (int i=0; i<preview.optionsViews.count; i++) {
